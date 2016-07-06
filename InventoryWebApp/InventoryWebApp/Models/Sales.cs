90006ebd7eb59d0ecfd6a9022d2006b5490a94b1
GreyFlyDev <gregnnylf94@gmail.com>
2016-07-06 14:02:51 -0400

    Initial + Sale, Restock, and Product Models

---
+++ InventoryWebApp/InventoryWebApp/Models/Sales.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InventoryWebApp.Models
+{
+    public class Sales
+    {
+        public int SalesId { get; set; }
+        public int QuantitySold { get; set; }
+
+        public int ProductId { get; set; }
+        public string UserId { get; set; }
+    }
+}
